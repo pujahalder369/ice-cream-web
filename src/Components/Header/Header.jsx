4b9a344486b7011132beb7640a56938974e28e89
puja <proy87830@gmail.com>
2026-08-31 13:53:22 +0530

    logout

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,15 @@
 import { IoIceCreamOutline } from 'react-icons/io5'
 import { MdLogin, MdOutlineShoppingCart } from 'react-icons/md'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Header.css'
+import { RiLogoutCircleRLine } from 'react-icons/ri';
 
 const Header = () => {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem("isLoggedIn");
+    navigate('/login');
+  }
   return (
     <div className='mb-20'>
       <div className='fixed z-50 w-full flex justify-between items-center bg-pink-300 py-4 px-6'>
@@ -14,11 +20,12 @@ const Header = () => {
         </h2>
         <div className='flex items-center gap-3'>
           <div className='bg-white shadow-lg rounded-full hover:shadow-xl hover:bg-pink-200 transition-all duration-300'>
-            <Link to={'/login'}>
+            <button onClick={handleLogout} className='tooltip'>
               <div className='flex items-center gap-3 text-xl p-3'>
-                <MdLogin size={25} />
+                <RiLogoutCircleRLine size={25} />
               </div>
-            </Link>
+              <span class="tooltiptext">LogOut</span>
+            </button>
           </div>
           <div className='bg-white shadow-lg rounded-full hover:shadow-xl hover:bg-pink-200 transition-all duration-300'>
             <Link to={'/place-order'}>
